7126f51406374813e58403b8a2e16acbf2e4f8aa
toooooodo <1042178105@qq.com>
2019-07-30 15:44:47 +0800

    complete train code, but it does not work

--- main.py
+++ main.py
@@ -7,22 +7,24 @@ from prepare_data import Triple
 device = torch.device('cuda')
 embed_dim = 50
 num_epochs = 20
-lr = 1e-2
+lr = 1e-4
 momentum = 0
 gamma = 1
-d_norm = 2
+d_norm = 1
 
 
 def main():
     dataset = Triple()
     loader = DataLoader(dataset, batch_size=32, shuffle=True)
-    transe = TranE(dataset.entity_num, dataset.relation_num).to(device)
+    transe = TranE(dataset.entity_num, dataset.relation_num, device, dim=embed_dim, d_norm=d_norm, gamma=gamma).to(
+        device)
     optimizer = optim.SGD(transe.parameters(), lr=lr, momentum=momentum)
     for epoch in range(num_epochs):
         # e <= e / ||e||
         entity_norm = torch.norm(transe.entity_embedding.weight.data, dim=1).view(-1, 1)
         _, entity_norm = torch.broadcast_tensors(transe.entity_embedding.weight.data, entity_norm)
         transe.entity_embedding.weight.data = transe.entity_embedding.weight.data / entity_norm
+        total_loss = 0
         for batch_idx, (pos, neg) in enumerate(loader):
             pos, neg = pos.to(device), neg.to(device)
             # pos: [batch_size, 3] => [3, batch_size]
@@ -32,14 +34,12 @@ def main():
             neg = torch.transpose(neg, 0, 1)
             # neg_head, neg_relation, neg_tail: [batch_size]
             neg_head, neg_relation, neg_tail = neg[0], neg[1], neg[2]
-            pos_head_and_relation, pos_tail, neg_head_and_relation, neg_tail = transe(pos_head, pos_relation, pos_tail,
-                                                                                      neg_head, neg_relation, neg_tail)
-            loss = torch.max(torch.Tensor([0]).to(device),
-                             gamma + torch.dist(pos_head_and_relation, pos_tail) - torch.dist(neg_head_and_relation,
-                                                                                              neg_tail))
-            print(loss)
-            break
-        break
+            loss = transe(pos_head, pos_relation, pos_tail, neg_head, neg_relation, neg_tail)
+            total_loss += loss.item()
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+        print(f"epoch {epoch+1}, loss = {total_loss/dataset.__len__()}")
 
 
 if __name__ == '__main__':
--- model.py
+++ model.py
@@ -1,17 +1,30 @@
 import torch
 import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader
+import torch.nn.functional as F
 from prepare_data import Triple
 import math
 
 
 class TranE(nn.Module):
-    def __init__(self, entity_num, relation_num, dim=50):
+    def __init__(self, entity_num, relation_num, device, dim=50, d_norm=2, gamma=1):
+        """
+        :param entity_num: number of entities
+        :param relation_num: number of relations
+        :param dim: embedding dim
+        :param device:
+        :param d_norm: measure d(h+l, t), either L1-norm or L2-norm
+        :param gamma: margin hyperparameter
+        """
         super(TranE, self).__init__()
+        self.dim = dim
+        self.d_norm = d_norm
+        self.device = device
+        self.gamma = torch.FloatTensor([gamma]).to(self.device)
         self.entity_embedding = nn.Embedding.from_pretrained(
-            torch.empty(entity_num, dim).uniform_(-6 / math.sqrt(dim), 6 / math.sqrt(dim)))
+            torch.empty(entity_num, self.dim).uniform_(-6 / math.sqrt(self.dim), 6 / math.sqrt(self.dim)))
         self.relation_embedding = nn.Embedding.from_pretrained(
-            torch.empty(entity_num, dim).uniform_(-6 / math.sqrt(dim), 6 / math.sqrt(dim)))
+            torch.empty(relation_num, self.dim).uniform_(-6 / math.sqrt(self.dim), 6 / math.sqrt(self.dim)))
         # l <= l / ||l||
         relation_norm = torch.norm(self.relation_embedding.weight.data, dim=1).view(-1, 1)
         _, relation_norm = torch.broadcast_tensors(self.relation_embedding.weight.data, relation_norm)
@@ -25,16 +38,24 @@ class TranE(nn.Module):
         :param neg_head: [batch_size]
         :param neg_relation: [batch_size]
         :param neg_tail: [batch_size]
-        :return: pos_head_and_relation: [batch_size, embed_dim]
-                 pos_tail: [batch_size, embed_dim]
-                 neg_head_and_relation: [batch_size, embed_dim]
-                 neg_tail: [batch_size, embed_dim]
+        :return: triples loss
         """
-        pos_head_and_relation = self.entity_embedding(pos_head) + self.relation_embedding(pos_relation)
-        pos_tail = self.entity_embedding(pos_tail)
-        neg_head_and_relation = self.entity_embedding(neg_head) + self.relation_embedding(neg_relation)
-        neg_tail = self.entity_embedding(neg_tail)
-        return pos_head_and_relation, pos_tail, neg_head_and_relation, neg_tail
+        pos_dis = self.entity_embedding(pos_head) + self.relation_embedding(pos_relation) - self.entity_embedding(
+            pos_tail)
+        neg_dis = self.entity_embedding(neg_head) + self.relation_embedding(neg_relation) - self.entity_embedding(
+            neg_tail)
+        # return pos_head_and_relation, pos_tail, neg_head_and_relation, neg_tail
+        return self.calculate_loss(pos_dis, neg_dis).requires_grad_()
+
+    def calculate_loss(self, pos_dis, neg_dis):
+        """
+        :param pos_dis: [batch_size, embed_dim]
+        :param neg_dis: [batch_size, embed_dim]
+        :return: triples loss: [batch_size]
+        """
+        distance_diff = self.gamma + torch.norm(pos_dis, p=self.d_norm, dim=1) - torch.norm(neg_dis, p=self.d_norm,
+                                                                                            dim=1)
+        return torch.sum(F.relu(distance_diff))
 
 
 if __name__ == '__main__':
